d53e81af062f72ab92743d11aec37050505e636c
Prince <princemondano123@gmail.com>
2025-11-30 16:32:03 -0500

    Just figured out how to get the file and store it in a string.

--- Employee Storing Data/Main.cpp
+++ Employee Storing Data/Main.cpp
@@ -6,4 +6,37 @@
 */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
+
+int main(int argc, char* argv[])
+{
+
+	std::cout << "argc: " << argc << std::endl;	
+	
+	std::cout << "argv[1]: " << argv[1] << std::endl;
+
+	//variables
+	std::ifstream myFile(argv[1]); //Opening the csv file
+	std::string holdthis; 
+
+
+	
+	//Reading The File
+	char test;
+	std::string hold;
+
+	while (myFile.get(test))
+	{
+		hold.append(1, test);
+		std::cout << hold << std::endl;
+		break;
+	}
+
+	
+	
+
+
+	myFile.close();
+}
